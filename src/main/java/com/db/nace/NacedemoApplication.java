5ac702cb56448cd0706ff6649f4f7cef292a4923
sharmamohit601 <46538154+sharmamohit601@users.noreply.github.com>
2021-11-18 15:18:07 +0530

    demo

---
+++ src/main/java/com/db/nace/NacedemoApplication.java
@@ -0,0 +1,13 @@
+package com.db.nace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NacedemoApplication.class, args);
+	}
+
+}
